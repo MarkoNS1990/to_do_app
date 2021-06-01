b824f5ba384dc9359221e08d238878be02c2b010
Marko Zecevic <marczecevic@gmail.com>
2021-06-01 11:02:47 +0200

    Add uuid as an id for each field and for id as project and task classes

--- src/project.js
+++ src/project.js
@@ -1,19 +1,23 @@
 import {addNewTask} from './task'
+import { v4 as uuidv4 } from 'uuid';
 let myProjects = []
 class Project{
     constructor(name){
         this.name = name
-        
+        this.tasks = []
+        this.id = uuidv4()
     }
+    
 
 }
 
-let myTasks = []
+
 class Task{
     constructor(name,date,priority){
         this.name = name
         this.date = date
         this.priority = priority
+        this.id=uuidv4()
     }
 }
 
@@ -33,53 +37,64 @@ const addNewProject = (()=>{
     addButton.addEventListener('click',()=>{
         const myProject = new Project(nameInput.value)
         myProjects.push(myProject)
-        console.log( myProject.__proto__.constructor.name);
+        
         const card = document.createElement('div')
         card.classList.add('card')
+        
+        const cardBodyId = uuidv4()
 
         myProjects.forEach((project)=>{
             card.innerHTML = `
             <div class='card-title'>
                 ${project.name}
-                <button class='btn btn-success addBtn'>Add</button>
+                <button class='btn btn-success addBtn' data=${myProject.id}>Add</button>
             </div>
-            <div class='card-body'>
+            <div class='card-body' data=${cardBodyId}>
                 
             </div>
             
         `
         })
 
-        
-        
         content.appendChild(card)
         nameInput.value=''
         
-        const addBtn = document.querySelector('.addBtn')
+        const addBtn = document.querySelector(`[data='${myProject.id}']`)
+        
         addBtn.addEventListener('click',()=>{
-            const cardBody = document.querySelector('.card-body')
+            console.log(addBtn);
+            console.log(myProject);
+            const submitId = uuidv4()
+            const inputId = uuidv4()
+            const priorityId = uuidv4()
+            const dateId = uuidv4()
+            const cardBody = document.querySelector(`[data='${cardBodyId}']`)
+
             cardBody.innerHTML = `
             <form type='submit'>   
-            <input type='text' placeholder='enter a task' class='form-control taskInput'>
-                <input type='date' class='dateInput'> 
-                <select name='priority' class='priority'>
+            <input type='text' placeholder='enter a task' class='form-control taskInput' data=${inputId}>
+                <input type='date' class='dateInput' data=${dateId}> 
+                <select name='priority' class='priority' data=${priorityId}>
                     <option value='low'>Low</option>
                     <option value='high'>High</option>
                 </select> 
-            <button type ='submit' class='btn btn-secondary submitForm'>Submit</button>
+            <button type ='submit' class='btn btn-secondary submitForm' data=${submitId}>Submit</button>
             </form> 
             `
-            const submitForm = document.querySelector('.submitForm')
-            const taskInput = document.querySelector('.taskInput')
-            const priority = document.querySelector('.priority')
-            const dateInput = document.querySelector('.dateInput')
+            const submitForm = document.querySelector(`[data='${submitId}']`)
+            const taskInput = document.querySelector(`[data='${inputId}']`)
+            const priority = document.querySelector(`[data='${priorityId}']`)
+            const dateInput = document.querySelector(`[data='${dateId}']`)
+            
             submitForm.addEventListener('click',(e)=>{
                 e.preventDefault()
+                
                 const myTask = new Task(taskInput.value,dateInput.value,priority.value)
-                myTasks.push(myTask)
+                
+                myProject.tasks.push(myTask)
                 setDisplayNone(taskInput,dateInput,priority,submitForm)
-                console.log(myTasks);
-                myTasks.forEach((task)=>{
+                
+                myProject.tasks.forEach((task)=>{
                     cardBody.innerHTML +=`
                     <p class='card-item'>${task.name} ${task.date} ${task.priority}</p>
                 `
@@ -99,6 +114,13 @@ const setDisplayNone = (name,date,priority,button)=>{
     date.style.display ='none'
     priority.style.display ='none'
     button.style.display = 'none'
+}
+
+const setDisplayBlock = (name,date,priority,button)=>{
+    name.style.display ='block'
+    date.style.display ='block'
+    priority.style.display ='block'
+    button.style.display = 'block'
 }
 
 
